1ab765d442fb2f45c54e0b1784e0ab8fb045fcb7
amanryzus <aman.ryzus@gmail.com>
2017-05-20 10:42:17 +0530

    Update prim's_algorithm.py

--- prim's_algorithm.py
+++ prim's_algorithm.py
@@ -1,22 +1,22 @@
-def prim(G,s):
-    q,p,visit={},{},[i for i in G]
-    for i in G:
+def prim(g,s):
+    q={}
+    p={}
+    visit=[i for i in g]
+    for i in g:
         q[i]=float('inf')
-    q[s],p[s]=0,None
     while q:
-        u=min(q.items(), key=lambda x: x[1])[0]
-        if u in q: del q[u]
-        print(u)
-        for v in G[u]:
-            if v in q and G[v][u]<q[v]:
+        u=min(q.items(),key=lambda x: x[1] )[0]
+        if u in q:del q[u]
+        for v in g[u]:
+            if v in q and g[u][v]<q[v]:
                 p[v]=u
-                q[v]=G[u][v]
+                q[v]=g[u][v]
     return p
-g={0: {1: 3, 3: 6, 2: 1},           #edge:{edge1:wt,egde2:wt........
-   1: {0: 3, 4: 3, 2: 5},
-   4: {1: 3, 5: 6, 2: 6},
-   5: {4: 6, 3: 2, 2: 4},
-   3: {5: 2, 0: 6, 2: 5},
-   2: {0: 1, 1: 5, 4: 6, 5: 4, 3: 5}
-   }
-print(prim(g,0))
+g={1:{2:4,4:8},2:{3:3,4:1},3:{5:8,4:7},4:{5:3},5:{}}
+path,a=prim(g,1),0
+for v1,v2 in path.items():
+    print(v2,"->",v1,"dist",g[v2][v1] if v2!=None else 0)
+    if v2!=None:
+        a=a+g[v2][v1]
+print(a)
+
